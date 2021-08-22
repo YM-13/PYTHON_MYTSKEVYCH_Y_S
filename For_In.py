8a756ba329f597ffd1950bd4a4b9e087a6570078
YM <exitospec1@gmail.com>
2021-08-22 21:56:01 +0300

    for_in_1

---
+++ For_In.py
@@ -0,0 +1,18 @@
+friend_name = 'Max'
+friends = ['Max', 'Leo', 'Kate']
+roles = ('admin', 'guest', 'user')
+
+if 'Max' in friends:
+    print('I have this friend')
+
+if 'M' in friend_name:
+    print('Letter M is in name of friend')
+
+# while
+i = 0
+while i < len(friends):
+    friend = friends[i]
+    print(friend)
+    i += 1
+
+# for
