57678249031d9a8e3021af8b853489b091b08aa8
YM <exitospec1@gmail.com>
2021-09-01 14:26:27 +0300

    hjhj

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 def my_func():
     data = input(' Введите числа, разделяя их запятой: ').split(',')
     s = 0
+    let = []
     c = len(data)
     for i in data:
         i = float(i)
         s = i + s
+
     return s
 print(my_func())
 
