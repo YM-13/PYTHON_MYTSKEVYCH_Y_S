7821cafcead1663504deaf41cea5fb3c6611b559
YM <exitospec1@gmail.com>
2021-09-05 15:05:25 +0300

    new_l-7

--- MY_5-1.v1.py
+++ MY_5-1.v1.py
@@ -3,3 +3,13 @@
 вводимые пользователем. Об окончании ввода данных будет свидетельствовать пустая
 строка.
 """
+inp = input('Input: ')
+
+while inp != '':
+    stro = inp
+    with open("hw1.txt", "a", encoding="utf-8") as your_t:
+        your_t.writelines(f"{stro}s\n")
+    inp = input('Input: ')
+with open("hw1.txt", "r", encoding="utf-8") as your_txt:
+    cont = your_txt.read()
+    print(f'Вы ввели следующее:\n {cont}')
