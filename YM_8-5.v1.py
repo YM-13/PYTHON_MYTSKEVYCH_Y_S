3522d3760709720e1b2bcea3c55ec52abcba1fff
YM <exitospec1@gmail.com>
2021-11-05 23:15:23 +0200

    8_n9

--- YM_8-5.v1.py
+++ YM_8-5.v1.py
@@ -4,50 +4,73 @@
 данных о наименовании и количестве единиц оргтехники, а также других данных, можно
 использовать любую подходящую структуру (например, словарь).
 """
-"""
-Создайте собственный класс-исключение, который должен проверять содержимое списка на
-наличие только чисел. Проверить работу исключения на реальном примере. Запрашивать у
-пользователя данные и заполнять список необходимо только числами. Класс-исключение
-должен контролировать типы данных элементов списка.
-Примечание: длина списка не фиксирована. Элементы запрашиваются бесконечно, пока
-пользователь сам не остановит работу скрипта, введя, например, команду «stop». При этом
-скрипт завершается, сформированный список с числами выводится на экран.
-Подсказка: для этого задания примем, что пользователь может вводить только числа и строки.
-Во время ввода пользователем очередного элемента необходимо реализовать проверку типа
-элемента. Вносить его в список, только если введено число. Класс-исключение должен не
-позволить пользователю ввести текст (не число) и отобразить соответствующее сообщение.
-При этом работа скрипта не должна завершаться.
-"""
+
+class Orgtehnika:
+    ci_orgteh = 'шт'
+    n_podcherc = '___________________________'
+    work_color = 'цветной/черно-белый'
+
+    def __init__(self, name_type, factory, model, year, articul, pce, color):
+        self.name_type = name_type
+        self.factory = factory
+        self.model = model
+        self.year = year
+        self.articul = articul
+        self.pce = pce
+        #        self.max_format = max_format
+        self.color = color
+
+    def carta_tovara(self):
+        print(f'КАРТОЧКА ТОВАРА\nНаименование: {self.name_type}\nПроизводитель: {self.factory}\n'
+              f'Модель: {self.model}\nРежим работы: {Orgtehnika.work_color}\n'
+              f'Цвет устройства: {self.color}\nГод выпуска: {self.year}\nАртикул: {self.articul}\n'
+              f'Остаток на складе: {self.pce} {Orgtehnika.ci_orgteh}\n{Orgtehnika.n_podcherc}')
 
 
-class MyException_num:
+class Printer(Orgtehnika):
+    def __init__(self, name_type, factory, model, year, articul, pce, color):
+        Orgtehnika.__init__(self, name_type, factory, model, year, articul, pce, color)
 
-    def __init__(self, data_str):
-        self.data_str = data_str
-
-    def in_list(data_str):
-        list_of_num = []
-        stop = ['stop']
-        lis = data_str.split(' ')
-        count = len(lis)
-        while count != stop:
-            n = lis.pop(0)
-            if n == n.isdigit():
-
-            if lis == stop:
-                break
-            for i in lis:
-                if i == stop:
-                    break
-                i = float(i)
-                list_of_num.append(i)
-                count -= 1
-            lis = (input('Введите данные: ')).split(' ')
-            count = len(lis)
-            continue
-        return list_of_num
+    def carta_tovara(self, sposob_pechati, max_paper):  # sposob_pechati
+        self.sposob_pechati = sposob_pechati
+        self.max_paper = max_paper
+        return (f'КАРТОЧКА ТОВАРА\nНаименование: {self.name_type}\nПроизводитель: {self.factory}\n'
+                f'Способ печати: {self.sposob_pechati}\nМодель: {self.model}\nРежим работы: {Orgtehnika.work_color}\n'
+                f'Формат бумаги: {self.max_paper}\nЦвет устройства: {self.color}\nГод выпуска: {self.year}\nАртикул: {self.articul}\n'
+                f'Остаток на складе: {self.pce} {Orgtehnika.ci_orgteh}\n{Orgtehnika.n_podcherc}')
 
 
-h = input('Введите данные: ')
-data_1 = MyException_num.in_list(h)
-print(data_1)
+class Scaner(Orgtehnika):
+    def __init__(self, name_type, factory, model, year, articul, pce, color):
+        Orgtehnika.__init__(self, name_type, factory, model, year, articul, pce, color)
+
+    def carta_tovara(self, tip_scanera, max_doc_format):
+        self.tip_scanera = tip_scanera
+        self.max_doc_format = max_doc_format
+        return (f'КАРТОЧКА ТОВАРА\nНаименование: {self.name_type}\nПроизводитель: {self.factory}\n'
+                f'Тип сканера: {self.tip_scanera}\nМодель: {self.model}\nРежим работы: {Orgtehnika.work_color}\n'
+                f'Формат документа: {self.max_doc_format}\nЦвет устройства: {self.color}\nГод выпуска: {self.year}\nАртикул: {self.articul}\n'
+                f'Остаток на складе: {self.pce} {Orgtehnika.ci_orgteh}\n{Orgtehnika.n_podcherc}')
+
+
+class Xerox(Orgtehnika):
+    def __init__(self, name_type, factory, model, year, articul, pce, color):
+        Orgtehnika.__init__(self, name_type, factory, model, year, articul, pce, color)
+
+    def carta_tovara(self, sposob_pechati, max_paper):  # sposob_pechati
+        self.sposob_pechati = sposob_pechati
+        self.max_paper = max_paper
+        return (f'КАРТОЧКА ТОВАРА\nНаименование: {self.name_type}\nПроизводитель: {self.factory}\n'
+                f'Способ печати: {self.sposob_pechati}\nМодель: {self.model}\nРежим работы: {Orgtehnika.work_color}\n'
+                f'Формат документа: {self.max_paper}\nЦвет устройства: {self.color}\nГод выпуска: {self.year}\nАртикул: {self.articul}\n'
+                f'Остаток на складе: {self.pce} {Orgtehnika.ci_orgteh}\n{Orgtehnika.n_podcherc}')
+
+
+prin_1 = Printer('Принтер', 'HP', 'CW86', '2021', '45-89', '99', 'Black')
+print(prin_1.carta_tovara('LASER', 'A3'))
+
+scan_1 = Scaner('Сканер', 'EPSON', 'YM897-18', '2020', '50-73', '5', 'White')
+print(scan_1.carta_tovara('Планшетный', 'A2'))
+
+xero_1 = Printer('Ксерокс', 'Canon', 'CaXe91-02', '2021', '46-15a', '7', 'Black')
+print(xero_1.carta_tovara('Струйный', 'A4'))
