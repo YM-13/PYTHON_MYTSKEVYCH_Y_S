4fd7d9d25efcb3eb97438aec67ebd54bcefe9683
YM <exitospec1@gmail.com>
2021-11-20 17:13:12 +0200

    OOP_N_3

---
+++ IA_1.py
@@ -0,0 +1,16 @@
+class 
+
+super().carta_tovara()
+
+str = super().carta_tovara()
+
+def __str__(self):
+    return ...
+
+print(obj)
+
+super().__str__()
+
+Orgtehnika.__init__(self, name_type, factory, model, year, articul, pce, color)
+
+super().__init__(self, name_type, factory, model, year, articul, pce, color)
