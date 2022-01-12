547df33f17c848e286be9298515946af87651d34
YM <exitospec1@gmail.com>
2022-01-12 19:53:58 +0200

    Python from Zero to Hero-4

--- UDEMY_ZERO_to_HERO_71.py
+++ UDEMY_ZERO_to_HERO_71.py
@@ -1,17 +1,89 @@
-class Animal():
+# class Animal():
+#
+#     def __init__(self):
+#         print("Animal Created")
+#
+#
+#     def who_am_i(self):
+#         print("I am an animal")
+#
+#     def eat(self):
+#         print("I am eating")
+#
+# #print(my_animal.__init__())
+#
+# class Dog(Animal):
+#     def __init__(self):
+#         Animal.__init__(self)
+#         print("Dog created")
+#
+#     def who_am_i(self):
+#         print("I am Dog!!!")
+#
+#     def bark(self):
+#         print("Woof")
+#
+# my_animal = Animal()
+# my_dog = Dog()
+# print(my_dog.bark())
 
-    def __init__(self):
-        print("Animal Created")
+# class Dog():
+#
+#     def __init__(self, name):
+#         self.name = name
 
+#     def speak(self):
+#         return self.name + " says Woof!"
+#
+#
+# class Cat():
 
-    def who_am_i(self):
-        print("I am an animal")
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def speak(self):
+#         return self.name + " says Meow!"
+#
+# nico = Dog("nico")
+# felix = Cat("felix")
+#
+# print(nico.speak())
 
-    def eat(self):
-        print("I am eating")
+# print(felix.speak())
+#
+# for pet in [nico, felix]:
+#
+#     print(type(pet))
+#     print(type(pet.speak()))
+#
+# def pet_speak(pet):
+#     print(pet.speak())
+#
+# pet_speak(nico)
+# pet_speak(felix)
 
-my_animal = Animal()
+class Animals():
 
-#print(my_animal.__init__())
+    def __init__(self, name):
+        self.name = name
 
-class Dog(Animal):
+    def speak(self):
+        raise NotImplementedError("Subclass must implement this abstract method")
+
+class Dog(Animals):
+
+    def speak(self):
+        return self.name + " says woof!"
+
+class Cat(Animals):
+
+    def speak(self):
+        return self.name + " says moew!"
+
+fido = Dog("Fido")
+
+isis = Cat("Isis")
+
+print(fido.speak())
+
+print(isis.speak())
